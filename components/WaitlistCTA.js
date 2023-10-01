8166e196f91aa2c792fd714dbc5f9515d41eb962
ADGroupEnterprises <abreham@adgroup.enterprises>
2023-10-01 15:38:46 -0700

    Waitlist page creation.

---
+++ components/WaitlistCTA.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import ButtonLead from "@/components/ButtonLead";
+
+
+const WaitlistCTA = () => {
+  return (
+    <section className="relative hero overflow-hidden min-h-screen">
+      <Image
+        src="https://source.unsplash.com/5QgIuuBxKwM"
+        alt="Background"
+        className="object-cover w-full"
+        fill
+      />
+      <div className="relative hero-overlay bg-neutral bg-opacity-70"></div>
+      <div className="relative hero-content text-center text-neutral-content p-8">
+        <div className="flex flex-col items-center max-w-xl p-8 md:p-0">
+          <h2 className="font-bold text-3xl md:text-5xl tracking-tight mb-8 md:mb-12">
+            Target the right leads and close deals faster
+          </h2>
+          <p className="text-lg opacity-80 mb-12 md:mb-16">
+            Don&apos;t waste time chasing the wrong clients 
+          </p>
+
+          <ButtonLead className="btn btn-primary btn-wide">Get ShipFast</ButtonLead>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WaitlistCTA;
