93b1a6f6034d982a4bed8baa83b3a19e4a58c314
Abreham Dadi <abreham@adgroup.enterprises>
2023-12-20 18:06:20 +0000

    MSPDEV-94 MSPDEV-101

--- Atlas/AppServices/WebScraping-LoadStaged-Data/functions/Load_WebScraped_LinkedInJobs_to_Companies.js
+++ Atlas/AppServices/WebScraping-LoadStaged-Data/functions/Load_WebScraped_LinkedInJobs_to_Companies.js
@@ -1,50 +1,88 @@
-// MongoDB Atlas Function Code
-// MongoDB Atlas Function Code
-exports = async function() {
-  // Reference to the MongoDB service
-  const mongodb = context.services.get("mongodb-atlas");
+// MongoDB Realm Function: Transform and load data from staging to companyData database
+exports = async function(changeEvent) {
+   try {
+    // MongoDB service client
+    const mongodb = context.services.get("mongodb-atlas");
   
-  // Reference to the source database
-  const sourceDb = mongodb.db('staging_webscraped');
-
-  // Read data from source collections
-  const companies = await sourceDb.collection('linkedinjobs_company').find().toArray();
-  const jobs = await sourceDb.collection('linkedinjobs_job').find().toArray();
-  const hardwareStack = await sourceDb.collection('linkedinjobs_jobHardwareStack').find().toArray();
-  const softwareStack = await sourceDb.collection('linkedinjobs_jobSoftwareStack').find().toArray();
+    // References to the staging database collections
+    const stagingDb = mongodb.db("staging_webscraped");
+    const companiesCollection = stagingDb.collection("linkedinjobs_company");
+    const jobsCollection = stagingDb.collection("linkedinjobs_job");
+    const jobHardwareStackCollection = stagingDb.collection("linkedinjobs_jobHardwareStack");
+    const jobSoftwareStackCollection = stagingDb.collection("linkedinjobs_jobSoftwareStack");
   
-  // Transform company data and associate hardware and software
-  let transformedCompanies = companies.map(company => {
-    // Find related jobs for the company
-    let companyJobs = jobs.filter(job => job.companyID.$oid === company._id.$oid);
-
-    // Extract and deduplicate hardware and software IDs from company's jobs
-    let hardwareIds = [...new Set(companyJobs.flatMap(job => job.jobHardwareStackID.map(hs => hs.$oid)))];
-    let softwareIds = [...new Set(companyJobs.flatMap(job => job.jobSoftwareStackID.map(ss => ss.$oid)))];
-
-    // Find and associate the hardware and software names
-    let associatedHardware = hardwareStack.filter(hw => hardwareIds.includes(hw._id.$oid));
-    let associatedSoftware = softwareStack.filter(sw => softwareIds.includes(sw._id.$oid));
-
-    return {
-      _id: company._id,
-      name: company.companyName,
-      headquartersCity: company.companyHQCity,
-      headquartersState: company.companyHQState,
-      employeeCount: parseInt(company.companyEmployeeCount.$numberInt),
-      industry: company.companyIndustry,
-      hardware: associatedHardware.map(hw => hw.hardwareName),
-      software: associatedSoftware.map(sw => sw.softwareName),
-    };
-  });
-
-  // Reference to the target database
-  const targetDb = mongodb.db('companyData');
+    // Reference to the production database collection
+    const productionDb = mongodb.db("companyData");
+    const transformedCompaniesCollection = productionDb.collection("transformed_companies");
   
-  // Write transformed data to target database
-  for (const company of transformedCompanies) {
-    await targetDb.collection('transformed_companies').insertOne(company);
+    // Helper function to fetch and aggregate hardware and software names
+    async function fetchAndAggregateHardwareSoftware(jobDocument) {
+      const hardwarePromises = jobDocument.jobHardwareStackID.map(id =>
+        jobHardwareStackCollection.findOne({ _id: id }).then(hardware => hardware.hardwareName)
+      );
+      const softwarePromises = jobDocument.jobSoftwareStackID.map(id =>
+        jobSoftwareStackCollection.findOne({ _id: id }).then(software => software.softwareName)
+      );
+  
+      const [hardwareNames, softwareNames] = await Promise.all([
+        Promise.all(hardwarePromises),
+        Promise.all(softwarePromises)
+      ]);
+  
+      return { hardwareNames, softwareNames };
+    }
+  
+    // The main function to transform and load the company data
+    async function transformAndLoadCompanyData(companyId) {
+      // Fetch the company data from the staging area
+      const companyData = await companiesCollection.findOne({ _id: companyId });
+      if (!companyData) {
+        console.error("Company not found with ID:", companyId);
+        return;
+      }
+  
+      // Fetch all job postings for this company
+      const jobDocuments = await jobsCollection.find({ companyID: companyId }).toArray();
+  
+      // Parallel fetching of all related hardware and software for each job
+      const jobsAggregationPromises = jobDocuments.map(async jobDocument => {
+        const { hardwareNames, softwareNames } = await fetchAndAggregateHardwareSoftware(jobDocument);
+        return { jobTitle: jobDocument.jobTitle, hardwareNames, softwareNames };
+      });
+  
+      // Wait for all job data to be aggregated
+      const aggregatedJobsData = await Promise.all(jobsAggregationPromises);
+  
+      // Prepare the final document to be inserted into the production collection
+      const transformedCompanyData = {
+        _id: companyData._id, // Use the original ObjectId of the company
+        name: companyData.companyName,
+        headquartersCity: companyData.companyHQCity,
+        headquartersState: companyData.companyHQState,
+        employeeCount: parseInt(companyData.companyEmployeeCount.$numberInt),
+        industry: companyData.companyIndustry,
+        hardware: aggregatedJobsData.flatMap(job => job.hardwareNames),
+        software: aggregatedJobsData.flatMap(job => job.softwareNames)
+      };
+  
+      // Upsert the transformed document into the production collection
+      await transformedCompaniesCollection.updateOne(
+        { _id: companyId },
+        { $set: transformedCompanyData },
+        { upsert: true }
+      );
+    }
+  
+    // Check the source of the change event and proceed with transformation if it's a job update
+    if (changeEvent.operationType === "insert" || changeEvent.operationType === "update") {
+      const jobDocument = changeEvent.fullDocument;
+      await transformAndLoadCompanyData(jobDocument.companyID);
+    }
+     return { success: true, message: "Data processed and upserted successfully" };
   }
-
-  return { result: "Data transformed and inserted successfully" };
+  catch (error) {
+    console.error("Error in function execution", error);
+    // Return an error object that is EJSON serializable
+    return { success: false, error: error.message };
+  }
 };
