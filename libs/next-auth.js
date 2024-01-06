d7e42ba05f8db1d9c24a4bb7846b0c93c8a5735a
Abreham Dadi <abreham@adgroup.enterprises>
2024-01-06 02:40:39 +0000

    Fix

--- libs/next-auth.js
+++ libs/next-auth.js
@@ -2,14 +2,9 @@ import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import config from "@/config";
-import AuthDB from "./connectToAuthDB";
-import AccountsDB from "./connectToAccountsDB";
+import connectMongo from "./connectToAuthDatabase";
 
-const addToUserDetailsCol = async (user) => {
-  const collection = AccountsDB.collection('user_details'); // Replace with your collection name
-  await collection.insertOne(user);  
-};
-
+connectToUserDataDatabase
 export const authOptions = {
   // Set any random key in .env.local
   secret: process.env.NEXTAUTH_SECRET,
@@ -30,7 +25,7 @@ export const authOptions = {
     }),
     // Follow the "Login with Email" tutorial to set up your email server
     // Requires a MongoDB database. Set MONOGODB_URI env variable.
-    ...(AuthDB
+    ...(connectMongo
       ? [
           EmailProvider({
             server: process.env.EMAIL_SERVER,
@@ -42,21 +37,7 @@ export const authOptions = {
   // New users will be saved in Database (MongoDB Atlas). Each user (model) has some fields like name, email, image, etc..
   // Requires a MongoDB database. Set MONOGODB_URI env variable.
   // Learn more about the model type: https://next-auth.js.org/v3/adapters/models
-  ...(AuthDB && { adapter: MongoDBAdapter(AuthDB) }),
-
-  callbacks: {
-    session: async ({ session }) => {
-      if (isNewUser) {
-        // Add the user to the second database
-        await addToUserDetailsCol({
-          email: session.user.email,
-          name: "",
-          company: "",
-        });
-      }
-      return true;
-    }
-  },
+  ...(connectMongo && { adapter: MongoDBAdapter(connectMongo) }),
 
   callbacks: {
     session: async ({ session, token }) => {
@@ -66,16 +47,13 @@ export const authOptions = {
       return session;
     },
   },
-
   session: {
     strategy: "jwt",
   },
-  
   theme: {
     brandColor: config.colors.main,
     // Add you own logo below. Recommended size is rectangle (i.e. 200x50px) and show your logo + name.
     // It will be used in the login flow to display your logo. If you don't add it, it will look faded.
     logo: `https://${config.domainName}/icon.png`,
   },
-  
 };
